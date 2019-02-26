089ecffc66941a68986ddb39500803e3b58777d3
Dominic Allemann <dominic.allemann@compass-security.com>
2019-02-26 15:04:34 +0100

    adding hidden service

---
+++ app/controllers/WeatherController.java
@@ -0,0 +1,39 @@
+package controllers;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.inject.Inject;
+
+import play.Logger;
+import play.libs.ws.WSClient;
+import play.libs.ws.WSRequest;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class WeatherController extends Controller
+{
+
+  private final WSClient              ws;
+
+  private static final Logger.ALogger LOGGER = Logger.of(WeatherController.class);
+
+  @Inject
+  WeatherController(
+                    WSClient ws)
+  {
+    this.ws = ws;
+
+  }
+
+  public Result forecast()
+  {
+
+    WSRequest request = ws.url("https://app-prod-ws.meteoswiss-app.ch/v1/plzDetail?plz=450000");
+    WSRequest complexRequest = request.setRequestTimeout(Duration.of(4000, ChronoUnit.MILLIS));
+    JsonNode returnBody = complexRequest.get().thenApply(response -> response.asJson()).toCompletableFuture().join();
+    return ok(returnBody).withHeader("Access-Control-Allow-Origin", "*");
+  }
+
+}
---
+++ unpack.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\7-Zip\7z.exe" x -y C:\privateWS\memebrancer\target\universal\memebrancer-1.2-SNAPSHOT.zip -oC:\privateWS\memebrancer\target\universal\
+
+explorer C:\privateWS\memebrancer\target\universal\memebrancer-1.2-SNAPSHOT
+pause
