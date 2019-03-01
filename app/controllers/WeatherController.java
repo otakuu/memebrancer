25fb1a2bbbe724bfcb9536345f533a2e739fef4d
Dominic Allemann <dominic.allemann@gmail.com>
2019-03-01 14:22:55 +0100

    new temperature endpoint

--- app/controllers/WeatherController.java
+++ app/controllers/WeatherController.java
@@ -12,28 +12,35 @@ import play.libs.ws.WSRequest;
 import play.mvc.Controller;
 import play.mvc.Result;
 
-public class WeatherController extends Controller
-{
+public class WeatherController extends Controller {
 
-  private final WSClient              ws;
+	private final WSClient ws;
 
-  private static final Logger.ALogger LOGGER = Logger.of(WeatherController.class);
+	private static final Logger.ALogger LOGGER = Logger.of(WeatherController.class);
 
-  @Inject
-  WeatherController(
-                    WSClient ws)
-  {
-    this.ws = ws;
+	@Inject
+	WeatherController(WSClient ws) {
+		this.ws = ws;
 
-  }
+	}
 
-  public Result forecast()
-  {
+	public Result forecast() {
 
-    WSRequest request = ws.url("https://app-prod-ws.meteoswiss-app.ch/v1/plzDetail?plz=450000");
-    WSRequest complexRequest = request.setRequestTimeout(Duration.of(4000, ChronoUnit.MILLIS));
-    JsonNode returnBody = complexRequest.get().thenApply(response -> response.asJson()).toCompletableFuture().join();
-    return ok(returnBody).withHeader("Access-Control-Allow-Origin", "*");
-  }
+		WSRequest request = ws.url("https://app-prod-ws.meteoswiss-app.ch/v1/plzDetail?plz=450000");
+		WSRequest complexRequest = request.setRequestTimeout(Duration.of(6000, ChronoUnit.MILLIS));
+		JsonNode returnBody = complexRequest.get().thenApply(response -> response.asJson()).toCompletableFuture()
+				.join();
+		return ok(returnBody).withHeader("Access-Control-Allow-Origin", "*");
+	}
+
+	public Result temperature() {
+
+		WSRequest request = ws.url(
+				"https://www.meteoschweiz.admin.ch/product/input/measured-values/messwerte-lufttemperatur-10min/stationsTable/stationsTable.messwerte-lufttemperatur-10min.de.json");
+		WSRequest complexRequest = request.setRequestTimeout(Duration.of(6000, ChronoUnit.MILLIS));
+		JsonNode returnBody = complexRequest.get().thenApply(response -> response.asJson()).toCompletableFuture()
+				.join();
+		return ok(returnBody).withHeader("Access-Control-Allow-Origin", "*");
+	}
 
 }
